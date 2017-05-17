b54030fe9feb4147a6b8d4ae47e2666191269285
apple11361 <apple113611361@gmail.com>
2017-05-17 20:30:34 +0800

    0.html button

--- Front_end/assets/js/img_table.js
+++ Front_end/assets/js/img_table.js
@@ -1,4 +1,7 @@
 
+var photo = 0;
+
+
 function change(a)
 {
 	var s = new Image();
@@ -10,4 +13,39 @@ function change(a)
 	}
 	
 	s.src = "../images/0/"+a+".jpg";
+	photo = a;
+}
+
+function right()
+{
+	photo = (photo+1)%5;
+	
+	var s = new Image();
+	s.onload = ch;
+	
+	function ch()
+	{
+		var m = document.getElementById("thumbnail");
+		m.src = s.src;
+	}
+	
+	s.src = "../images/0/"+photo+".jpg";
+	
+}
+
+function left()
+{
+	photo = (photo+4)%5;
+	
+	var s = new Image();
+	s.onload = ch;
+	
+	function ch()
+	{
+		var m = document.getElementById("thumbnail");
+		m.src = s.src;
+	}
+	
+	s.src = "../images/0/"+photo+".jpg";
+	
 }
